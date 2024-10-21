baa1eb9ad3ba4dd5b2db417c72f02d7a647649c2
Murilo Brenner <cerebrox@gmail.com>
2024-10-21 15:01:16 -0300

    UPDATE

--- PY/EXC_EXCLUIR MATRICULA.py
+++ PY/EXC_EXCLUIR MATRICULA.py
@@ -3,24 +3,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 from db_login import login, driver
-from db_arquivos import elaboration
+from db_arquivos import elaboration, teste
+import csv
 
 df = pd.read_csv(elaboration)
 login()
 
-for i in df.index:
-    driver.get("http://gsan.caema.ma.gov.br:8080/gsan/exibirFiltrarClienteAction.do?menu=sim")
-    cod_cliente = int(df['COD_CLIENTE'][i])
-    driver.find_element(By.NAME, "codigoClienteFiltro").send_keys(cod_cliente)
-    driver.find_element(By.NAME, "atualizarFiltro").click()    
-    driver.find_element(By.XPATH, "//input[@value='Filtrar']").click()    
-    driver.find_element(By.XPATH, "//input[@type='checkbox' and @name='idRegistrosRemocao' and @value='{}']".format(cod_cliente)).click()    
-    driver.find_element(By.XPATH, "//input[@value='Remover']").click()
-    popup = driver.switch_to.alert
-    popup.accept()
-    try:
-        msg_ok = driver.find_element(By.XPATH, "/html/body/table[2]/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/div/span").text
-        print(i+1, cod_cliente, msg_ok)
-    except:
-        msg_er = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
-        print(i+1, cod_cliente, msg_er)
+header = ['#', 'MATRICULA', 'MSG']
+
+with open(teste, mode="w", newline="") as teste:
+    escritor = csv.writer(teste)
+    escritor.writerow(header)
+
+    for i in df.index:
+        driver.get("http://gsan.caema.ma.gov.br:8080/gsan/exibirFiltrarImovelAction.do?menu=sim")
+        matricula = int(df['MATRICULA'][i])
+        driver.find_element(By.NAME, "matriculaFiltro").send_keys(matricula)
+        driver.find_element(By.NAME, "atualizarFiltro").click()
+        driver.find_element(By.XPATH, "//input[@value='Filtrar']").click()
+        try:
+            msg_er = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
+            linha = i+1, matricula, msg_er
+            escritor.writerow(linha)            
+        except:                 
+            driver.find_element(By.XPATH, "//input[@type='checkbox' and @name='idRegistrosRemocao']").click()    
+            driver.find_element(By.XPATH, "//input[@value='Remover']").click()
+            popup = driver.switch_to.alert
+            popup.accept()
+            try:
+                msg_ok = driver.find_element(By.XPATH, "/html/body/table[2]/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/div/span").text
+                linha = i+1, matricula, msg_ok
+                escritor.writerow(linha)
+            except:
+                msg_er = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
+                linha = i+1, matricula, msg_er
+                escritor.writerow(linha)
