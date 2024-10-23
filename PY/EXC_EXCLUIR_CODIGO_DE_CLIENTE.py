7c0ec07abc8aeaccd5b8cd0d1fa89557962aad85
Murilo Brenner Costa Silva <cerebrox@gmail.com>
2024-10-23 09:43:35 -0300

    ATUALIZACAO ERROS REPARADOS - 23-10-2024

--- PY/EXC_EXCLUIR_CODIGO_DE_CLIENTE.py
+++ PY/EXC_EXCLUIR_CODIGO_DE_CLIENTE.py
@@ -13,14 +13,19 @@ for i in df.index:
     cod_cliente = int(df['COD_CLIENTE'][i])
     driver.find_element(By.NAME, "codigoClienteFiltro").send_keys(cod_cliente)
     driver.find_element(By.NAME, "atualizarFiltro").click()    
-    driver.find_element(By.XPATH, "//input[@value='Filtrar']").click()    
-    driver.find_element(By.XPATH, "//input[@type='checkbox' and @name='idRegistrosRemocao' and @value='{}']".format(cod_cliente)).click()    
-    driver.find_element(By.XPATH, "//input[@value='Remover']").click()
-    popup = driver.switch_to.alert
-    popup.accept()
+    driver.find_element(By.XPATH, "//input[@value='Filtrar']").click()
     try:
-        msg_ok = driver.find_element(By.XPATH, "/html/body/table[2]/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/div/span").text
-        print(i+1, cod_cliente, msg_ok)
-    except:
         msg_er = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
-        print(i+1, cod_cliente, msg_er)
+        print(i+1, cod_cliente, msg_er)            
+    except:
+        driver.find_element(By.XPATH, "//input[@type='checkbox' and @name='idRegistrosRemocao' and @value='{}']".format(cod_cliente)).click()
+        time.sleep(1111)    
+        driver.find_element(By.XPATH, "//input[@value='Remover']").click()
+        popup = driver.switch_to.alert
+        popup.accept()
+        try:
+            msg_ok = driver.find_element(By.XPATH, "/html/body/table[2]/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/div/span").text
+            print(i+1, cod_cliente, msg_ok)
+        except:
+            msg_er = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
+            print(i+1, cod_cliente, msg_er)
