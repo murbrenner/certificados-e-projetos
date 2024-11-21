0918fd52e77d86109253e0fd9f153408532121fa
Murilo Brenner Costa Silva <cerebrox@gmail.com>
2024-11-21 17:37:09 -0300

    EXPEDIENTE 21-11-2024

--- PY/PROGRAM_DESPROGFISC_2024.py
+++ PY/PROGRAM_DESPROGFISC_2024.py
@@ -4,7 +4,7 @@ import pandas as pd
 from db_login import login, driver
 from db_arquivos import desprog_fisc, elaboration, prog_fisc, teste
 
-df = pd.read_csv(teste)
+df = pd.read_csv(elaboration)
 login()
 
 for i in df.index:
