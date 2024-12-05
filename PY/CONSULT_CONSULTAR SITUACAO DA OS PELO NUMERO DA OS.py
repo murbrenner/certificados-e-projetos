e6aabcc350a60b0dafe249950f6abe7cb89ac8de
Murilo Brenner Costa Silva <cerebrox@gmail.com>
2024-12-05 17:34:45 -0300

    EXPEDIENTE 05-12-2024

--- PY/CONSULT_CONSULTAR SITUACAO DA OS PELO NUMERO DA OS.py
+++ PY/CONSULT_CONSULTAR SITUACAO DA OS PELO NUMERO DA OS.py
@@ -10,15 +10,20 @@ driver.get("http://gsan.caema.ma.gov.br:8080/gsan/exibirFiltrarOrdemServicoActio
 driver.find_element(By.NAME, 'numeroOS').send_keys(str(df['OS'][0]))
 driver.find_element(By.XPATH, "//input[@value='Filtrar']").click()
 
+
 for i in df.index:
-    driver.find_element(By.NAME, 'numeroOSParametro').send_keys(str(df['OS'][i]))
-    driver.find_element(By.XPATH, "//input[@value='Pesquisar']").click()
-    try:
-        situacao = driver.find_element(By.NAME, "situacaoOS").get_attribute('value')
-        print(i + 1, str(df['OS'][i]), situacao)
-    except:
-        msg1 = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
-        print(i + 1, str(df['OS'][i]), msg1)
-        driver.find_element(By.XPATH, "//input[@value='Voltar']").click()
-        driver.find_element(By.NAME, 'numeroOSParametro').clear()
-        pass
+    os = int(df['OS'][i])
+    if os != 0:
+        driver.find_element(By.NAME, 'numeroOSParametro').send_keys(os)
+        driver.find_element(By.XPATH, "//input[@value='Pesquisar']").click()
+        try:
+            situacao = driver.find_element(By.NAME, "situacaoOS").get_attribute('value')
+            print(i + 1, str(df['OS'][i]), situacao)
+        except:
+            msg1 = driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td/table[3]/tbody/tr[1]/td[2]/span").text
+            print(i + 1, str(df['OS'][i]), msg1)
+            driver.find_element(By.XPATH, "//input[@value='Voltar']").click()
+            driver.find_element(By.NAME, 'numeroOSParametro').clear()
+            pass
+    if os == 0:
+        print(i + 1, str(df['OS'][i]), "OS INVÁLIDA")
